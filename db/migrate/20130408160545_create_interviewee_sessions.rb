9eca35c82293fb10bac0948914586ad9b403de3d
Michael Probber <mprobber@andrew.cmu.edu>
2013-04-29 10:04:14 -0400

    updated migrations

--- db/migrate/20130408160545_create_interviewee_sessions.rb
+++
@@ -1,9 +0,0 @@
-class CreateIntervieweeSessions < ActiveRecord::Migration
-  def change
-    create_table :interviewee_sessions do |t|
-      t.string :random
-      t.string :name
-      t.timestamps
-    end
-  end
-end
